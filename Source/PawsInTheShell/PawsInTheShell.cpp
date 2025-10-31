32c8067ab40d882f5d0b47e44aa60ef2ef7ccfb7
marcosecchi <loziosecchi@gmail.com>
2025-10-31 11:33:28 +0100

    Initial commit

---
+++ Source/PawsInTheShell/PawsInTheShell.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PawsInTheShell.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PawsInTheShell, "PawsInTheShell" );
