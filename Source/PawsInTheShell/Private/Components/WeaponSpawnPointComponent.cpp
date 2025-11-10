829da9c701e048e99147a79444e8686668e08046
marcosecchi <loziosecchi@gmail.com>
2025-11-10 18:09:43 +0100

    Add base weapon component.

---
+++ Source/PawsInTheShell/Private/Components/WeaponSpawnPointComponent.cpp
@@ -0,0 +1,16 @@
+﻿// Mastering Unreal Engine 5 Game Development with C++ Scripting
+// Packt Publishing 2025
+// Author: Marco Secchi (https://github.com/marcosecchi)
+
+#include "Components/WeaponSpawnPointComponent.h"
+
+UWeaponSpawnPointComponent::UWeaponSpawnPointComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UWeaponSpawnPointComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
---
+++ Source/PawsInTheShell/Public/Components/WeaponSpawnPointComponent.h
@@ -0,0 +1,26 @@
+﻿// Mastering Unreal Engine 5 Game Development with C++ Scripting
+// Packt Publishing 2025
+// Author: Marco Secchi (https://github.com/marcosecchi)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ArrowComponent.h"
+#include "WeaponSpawnPointComponent.generated.h"
+
+
+UCLASS(Blueprintable, BlueprintType, ClassGroup=("Paws In The Shell"), meta=(BlueprintSpawnableComponent))
+class PAWSINTHESHELL_API UWeaponSpawnPointComponent : public UArrowComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UWeaponSpawnPointComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:
+};
