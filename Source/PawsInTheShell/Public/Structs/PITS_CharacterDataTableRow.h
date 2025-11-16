6e2ff3c5be4702188633854c7d5b94c3969eee54
marcosecchi <loziosecchi@gmail.com>
2025-11-16 16:28:57 +0100

    Improve character stats.

--- Source/PawsInTheShell/Public/Structs/PITS_CharacterDataTableRow.h
+++ Source/PawsInTheShell/Public/Structs/PITS_CharacterDataTableRow.h
@@ -18,7 +18,10 @@ struct FPITS_CharacterDataTableRow : public FTableRowBase
 
 	UPROPERTY(EditAnywhere, meta=(ToolTip="Name of the character displayed in game"))
 	FText CharacterName;
-	
+
+	UPROPERTY(EditAnywhere, meta=(ToolTip="Character role in the team"))
+	FText CharacterSpeciality;
+
 	UPROPERTY(EditAnywhere, meta=(ToolTip="Maximum health reachable by the character"))
 	float MaxHealth = 100.0f;
 
