358402d16ea93a6a707dd0094c9aadaa7f582c7f
marcosecchi <752514+marcosecchi@users.noreply.github.com>
2026-04-07 19:24:42 +0200

    Move tests to dedicated folder.

---
+++ Source/PawsInTheShell/Private/Tests/Helpers/PITS_TestPoolableActor.cpp
@@ -0,0 +1,32 @@
+﻿// Mastering Unreal Engine 5 Game Development with C++ Scripting
+// Packt Publishing 2025
+// Author: Marco Secchi (https://github.com/marcosecchi)
+
+#include "Tests/Helpers/PITS_TestPoolableActor.h"
+#include "Utils/PITS_Logs.h"
+
+APITS_TestPoolableActor::APITS_TestPoolableActor()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void APITS_TestPoolableActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	SetActorHiddenInGame(true);
+}
+
+void APITS_TestPoolableActor::HandleAcquire_Implementation()
+{
+	SetActorHiddenInGame(false);
+
+	UE_LOG(LogPITS, Log, TEXT("'%s' Acquired from pool."), *GetNameSafe(this));
+}
+
+void APITS_TestPoolableActor::HandleRelease_Implementation()
+{
+	SetActorHiddenInGame(true);
+
+	UE_LOG(LogPITS, Log, TEXT("'%s' Released to pool."), *GetNameSafe(this));
+}
---
+++ Source/PawsInTheShell/Public/Tests/Helpers/PITS_TestPoolableActor.h
@@ -0,0 +1,30 @@
+﻿// Mastering Unreal Engine 5 Game Development with C++ Scripting
+// Packt Publishing 2025
+// Author: Marco Secchi (https://github.com/marcosecchi)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interfaces/PITS_PooledObjectInterface.h"
+#include "PITS_TestPoolableActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PAWSINTHESHELL_API APITS_TestPoolableActor : public AActor, public IPITS_PooledObjectInterface
+{
+	GENERATED_BODY()
+	
+public:
+	APITS_TestPoolableActor();
+
+protected:
+	virtual void BeginPlay() override;
+
+// IPITS_PooledObjectInterface implementations
+public:
+	virtual void HandleAcquire_Implementation() override;
+	virtual void HandleRelease_Implementation() override;
+
+};
